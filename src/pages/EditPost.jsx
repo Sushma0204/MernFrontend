c141d4ab11b3f4cc92d0e112d9863670d57d914a
Sushma <98072240+Sushma0204@users.noreply.github.com>
2023-11-22 12:56:37 +0530

    Update EditPost.jsx

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -21,7 +21,7 @@ const EditPost = () => {
 
   const fetchPost = async () => {
     try {
-      const res = await axios.get("https://back-end-api-5fjl.onrender.com/api/posts/" + postId)
+      const res = await axios.get("https://backend-api-zfle.onrender.com/api/posts/" + postId)
       setTitle(res.data.title)
       setDesc(res.data.desc)
       setFile(res.data.photo)
@@ -52,7 +52,7 @@ const EditPost = () => {
       // console.log(data)
       //img upload
       try {
-        const imgUpload = await axios.post("https://back-end-api-5fjl.onrender.com/api/upload", data)
+        const imgUpload = await axios.post("https://backend-api-zfle.onrender.com/api/upload", data)
         // console.log(imgUpload.data)
       }
       catch (err) {
@@ -62,7 +62,7 @@ const EditPost = () => {
     //post upload
 
     try {
-      const res = await axios.put("https://back-end-api-5fjl.onrender.com/api/posts/" + postId, post, { withCredentials: true })
+      const res = await axios.put("https://backend-api-zfle.onrender.com/api/posts/" + postId, post, { withCredentials: true })
       navigate("/posts/post/" + res.data._id)
       // console.log(res.data)
 
